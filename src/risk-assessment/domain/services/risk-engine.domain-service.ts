65ecb9565c742179b5584ec414dc6e9587c92fe1
menescal <danielmenescal@bluems.com>
2026-05-28 17:38:27 -0300

    refactor: change to make the engine agnostic to the rules

--- src/risk-assessment/domain/services/risk-engine.domain-service.ts
+++ src/risk-assessment/domain/services/risk-engine.domain-service.ts
@@ -1,28 +1,12 @@
+import { Injectable } from '@nestjs/common';
 import { RuleInterface } from '../rules/rule.interface';
 import { RiskContext } from '../entities/risk-context.entity';
 import { TransportOperation } from '../value-objects/transport-operation.vo';
-import { CargoTypeRule } from '../rules/cargo-type.rule';
-import { WeatherRule } from '../rules/weather.rule';
-import { TransportHistoryRule } from '../rules/transport-history.rule';
-import { CargoValueInsuranceRule } from '../rules/cargo-value-insurance.rule';
-import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class RiskEngineDomainService {
-  // Aqui e a lista com fluxo na ordem que tem que ser pra nao dar pau
-  private readonly rules: RuleInterface[] = [
-    new CargoTypeRule(),
-    new WeatherRule(),
-    new TransportHistoryRule(),
-    new CargoValueInsuranceRule(),
-  ];
+  constructor(private readonly rules: RuleInterface[]) {}
 
-  /**
-   * Avalia o risco de uma operação de transporte com base nas regras implementadas.
-   *
-   * @param transportOperation Operação de transporte a ser avaliada.
-   * @returns Contexto de risco com o nível de risco final e as razões e recomendações.
-   */
   evaluateTransportOperation(
     transportOperation: TransportOperation,
   ): RiskContext {
@@ -31,7 +15,6 @@ export class RiskEngineDomainService {
     for (const rule of this.rules) {
       rule.evaluate(transportOperation, riskContext);
 
-      //após cada regra executar, o engine verifica se o contexto foi marcado como final
       if (riskContext.isFinal()) {
         break;
       }
--- src/risk-assessment/risk-assessment.module.ts
+++ src/risk-assessment/risk-assessment.module.ts
@@ -2,9 +2,31 @@ import { Module } from '@nestjs/common';
 import { RiskAssessmentController } from './infrastructure/controllers/risk-assessment.controller';
 import { EvaluateTransportRiskUseCase } from './application/use-cases/evaluate-transport-risk.use-case';
 import { RiskEngineDomainService } from './domain/services/risk-engine.domain-service';
+import { CargoValueInsuranceRule } from './domain/rules/cargo-value-insurance.rule';
+import { TransportHistoryRule } from './domain/rules/transport-history.rule';
+import { WeatherRule } from './domain/rules/weather.rule';
+import { CargoTypeRule } from './domain/rules/cargo-type.rule';
+import { RuleInterface } from './domain/rules/rule.interface';
 
 @Module({
   controllers: [RiskAssessmentController],
-  providers: [RiskEngineDomainService, EvaluateTransportRiskUseCase],
+  providers: [
+    CargoTypeRule,
+    WeatherRule,
+    TransportHistoryRule,
+    CargoValueInsuranceRule,
+    {
+      provide: RiskEngineDomainService,
+      useFactory: (...rules: RuleInterface[]) =>
+        new RiskEngineDomainService(rules),
+      inject: [
+        CargoTypeRule,
+        WeatherRule,
+        TransportHistoryRule,
+        CargoValueInsuranceRule,
+      ],
+    },
+    EvaluateTransportRiskUseCase,
+  ],
 })
 export class RiskAssessmentModule {}
